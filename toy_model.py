f38858e33a58fef1be789981155382023b595df3
wanghj20 <100277793+wanghj20@users.noreply.github.com>
2025-09-13 23:52:37 +0800

    Update toy_model.py

--- toy_model.py
+++ toy_model.py
@@ -1,1 +1,13 @@
 
+# func.py
+import numpy as np
+
+def make_exp_linear(dim):
+   
+    lambdas = np.array([1.0 / (i**2) for i in range(1, dim + 1)], dtype=float)
+
+    def f(x):
+        
+        return float(np.exp(np.sum(lambdas * x)))
+
+    return f, lambdas
